58db6b7dd98b3ce9af97e17052d74adaf7d2d8fb
gugitgugit <rnwnsgur28@gmail.com>
2025-02-10 21:05:23 +0900

    CRA

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from './sections/Header';
+import Apply from './sections/Apply';
+import Introduction from './sections/Introduction';
+import Track from './sections/Track';
+import Activity from './sections/Activity';
+import Schedule from './sections/Schedule';
+import Question from './sections/Question';
+import Footer from './sections/Footer';
+import styled from 'styled-components';
+
+const Home = () => {
+  return (
+    <Container>
+      <Header />
+      <Apply />
+      <Introduction />
+      <Track />
+      <Activity />
+      <Schedule />
+      <Question />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 200px;
+`;
