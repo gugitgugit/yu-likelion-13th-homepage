0a84afc605ccfda27afa83fafe094f90d9b21a7a
jihyun132 <jihyun.q.s@gmail.com>
2025-02-13 03:07:31 +0900

    design: activity  이미지 설정 및 그라데이션 설정

--- src/sections/Activity.js
+++ src/sections/Activity.js
@@ -1,37 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import Session from '../assets/activity/session.png';
+import NetworkingDay from '../assets/activity/networking.png';
+import Ideathon from '../assets/activity/ideathon.png';
+import Study from '../assets/activity/study.png';
+import Hackathon from '../assets/activity/hackathon.png';
+import DemoDay from '../assets/activity/demoday.png';
 
 const Activity = () => {
   const activityList = [
     {
       id: 1,
       title: "1학기 정기 세션",
-      img: "",
+      img: Session,
     },
     {
       id: 2,
       title: "네트워킹 데이",
-      img: "",
+      img: NetworkingDay,
     },
     {
       id: 3,
       title: "아이디어톤",
-      img: "",
+      img: Ideathon,
     },
     {
       id: 4,
       title: "스터디",
-      img: "",
+      img: Study,
     },
     {
       id: 5,
       title: "해커톤",
-      img: "",
+      img: Hackathon,
     },
     {
       id: 6,
       title: "데모데이",
-      img: "",
+      img: DemoDay,
     },
   ];
   return (
@@ -39,10 +45,12 @@ const Activity = () => {
       <Title>주요 활동</Title>
       <ActivityContainer>
         {activityList.map(activity => (
-          <ActivityBox key={activity.id}>
-            {/* <ActivityImg/> */}
+          <ActivityContent 
+            key={activity.id}
+            bgimg={activity.img}
+          >
             <ActivityTitle>{activity.title}</ActivityTitle>
-          </ActivityBox>
+          </ActivityContent>
         ))}
       </ActivityContainer>
     </Wrapper>
@@ -65,16 +73,18 @@ const Title = styled.div`
 const ActivityContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 40px;
+  gap: 35px;
 `;
-const ActivityBox = styled.div`
-  border: 1px solid red;
-  border-radius: 45px;
+const ActivityContent = styled.div`
+  background-image: 
+  linear-gradient(rgba(79, 39, 21, 0.8), transparent), 
+  url(${props => props.bgimg});
+  background-size: cover;
+  border: 1.2px solid rgba(255, 255, 255, 0.5);
+  border-radius: 40px;
   width: 450px;
   height: 280px;
 `;
-// const ActivityImg = styled.img`
-// `;
 const ActivityTitle = styled.div`
   padding: 30px;
   font-size: 22px;
